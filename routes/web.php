9dcd488d63c356ca025b39e7437c284878e6e60c
tugelsikile <52416894+tugelsikile@users.noreply.github.com>
2020-08-28 20:17:45 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware'=>'auth'],function (){
+    Route::get('/','HomeController@index');
+});
